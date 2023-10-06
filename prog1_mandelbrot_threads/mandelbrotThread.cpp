d6fc09f8e1714656a667e078238f4de078c7cd3b
Paris Franz <franzpl@stanford.edu>
2023-10-05 22:32:48 -0700

    2 threads

--- prog1_mandelbrot_threads/mandelbrotThread.cpp
+++ prog1_mandelbrot_threads/mandelbrotThread.cpp
@@ -36,7 +36,7 @@ void workerThreadStart(WorkerArgs * const args) {
     // half of the image and thread 1 could compute the bottom half.
     int split_totalRows = args->height / args->numThreads;
     int split_startRow =  split_totalRows* args->threadId; 
-    if (args->height < startrow + 2*split_totalRows){
+    if (args->height < split_startRow + 2*split_totalRows){
         split_totalRows = args->height - split_startRow;
     }
     mandelbrotSerial(args->x0, args->y0, args->x1, args->y1, args->width, args->height, split_startRow, split_totalRows, args->maxIterations, args->output);
