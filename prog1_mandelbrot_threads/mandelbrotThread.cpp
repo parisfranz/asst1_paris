8e19d59f2718a3a54b4e488c3b92ff79f86e5436
Paris Franz <franzpl@stanford.edu>
2023-10-05 22:15:29 -0700

    2 threads

--- prog1_mandelbrot_threads/mandelbrotThread.cpp
+++ prog1_mandelbrot_threads/mandelbrotThread.cpp
@@ -37,6 +37,7 @@ void workerThreadStart(WorkerArgs * const args) {
     int split_totalRows = args->height / args->numThreads;
     int split_startRow = args->split_totalRows* args->threadId; 
     mandelbrotSerial(args->x0, args->y0, args->x1, args->y1, args->width, args->height, split_startRow, split_totalRows, args->maxIterations, args->output_serial);
+    printf('%d',split_totalRows);
     printf("Hello world from thread %d\n", args->threadId);
 }
 
